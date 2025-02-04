3e052cc19ed647fef1ddbaddeb2136ff707e6cb8
ElisaSonera <elisa.laatu@gmail.com>
2025-02-04 14:58:36 +0200

    osa 3c tehtävät 3.13-3.17

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan"); // 3.7 morgan käyttöön npm installin jälkeen
 const cors = require("cors");
 const app = express();
-const Person = require("./models/person"); // 3.13 mongoose
+const Person = require("./models/person");
 
 app.use(express.json());
 app.use(express.static("dist"));
@@ -14,40 +15,17 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 ); // 3.8 tiny + body
 
-
-// let persons = [
-//   {
-//     id: "1",
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: "2",
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: "3",
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: "4",
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ];
-
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
+  Person.countDocuments({}).then((count) => {
+    response.send(
+      `<p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>`
-  );
+    );
+  });
 });
 
 app.get("/api/persons", (request, response) => {
@@ -56,32 +34,38 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end(); // 3.3 Kai tämä on se "asianmukainen" statuskoodi?
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end(); // 3.3 Kai tämä on se "asianmukainen" statuskoodi?
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   // 3.4 "Numerotiedon yksilöivä URL" on siis kai id url?
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
+  // const id = request.params.id;
+  // persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = (persons) => {
-  const randomId = Math.floor(Math.random() * 1000) + 1; //3.5 ehkä 1-1000 on riittävän iso arvoväli?
-  if (persons.some((person) => person.id === randomId)) {
-    //varmistetaan, että listalla ei ole jo samalla ID:llä henkilöä
-    return generateId(persons); //rekursio jos on!
-  }
-  return String(randomId);
-};
+// const generateId = (persons) => {
+//   const randomId = Math.floor(Math.random() * 1000) + 1; //3.5 ehkä 1-1000 on riittävän iso arvoväli?
+//   if (persons.some((person) => person.id === randomId)) {
+//     //varmistetaan, että listalla ei ole jo samalla ID:llä henkilöä
+//     return generateId(persons); //rekursio jos on!
+//   }
+//   return String(randomId);
+// };
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -93,25 +77,66 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (persons.some((person) => person.name === body.name)) {
-    // 3.6 error jos nimi on jo luottelossa
-    return response.status(409).json({
-      error: "name must be unique",
-    });
-  }
+  Person.findOne({ name: body.name }).then((result) => {
+    if (result) {
+      return response.status(409).json({
+        error: "name must be unique",
+      });
+    } else {
+      const person = new Person({
+        name: body.name,
+        number: body.number,
+        //id: generateId(persons), -- ilmeisesti mongoDB generoi id:n jatkossa?
+      });
+
+      person.save().then((savedPerson) => {
+        response.json(savedPerson);
+      });
+    }
+  });
+
+  // if (persons.some((person) => person.name === body.name)) {
+  //   // 3.6 error jos nimi on jo luottelossa
+  //   return response.status(409).json({
+  //     error: "name must be unique",
+  //   });
+  // }
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
 
   const person = {
     name: body.name,
     number: body.number,
-    id: generateId(persons),
   };
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-const PORT = process.env.PORT || 3001;
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
